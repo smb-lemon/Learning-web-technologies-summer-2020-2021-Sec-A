79ed11e56b572eb55aef1bfca97d128b3627a222
Sm Bakibillah Lemon <66021119+smb-lemon@users.noreply.github.com>
2021-06-20 23:11:52 +0600

    first

---
+++ LAB_TASK_05_SESSION/public_home.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PUBLIC HOME</title>
+</head>
+<body>
+	<form method="post" action=""></form>
+	<table border="1" width="50%">
+		<tr>
+			<td width="50%">
+				Company
+			</td>
+			<td align="right"><a href="">Home</a>|<a href="">Login</a>|<a href="registration.php" >Registration</a></td>
+		</tr>
+		<tr>
+			<td colspan="2" valign="top"><h3> Welcome To XCompany</h3></td>
+		</tr>
+		<tr><td colspan="2" align="center">Copy right 2017</td></tr>
+	</table>
+
+	
+</body>
+</html>
