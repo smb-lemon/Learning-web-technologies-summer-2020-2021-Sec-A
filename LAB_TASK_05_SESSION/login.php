aab785e844cce9493a8bbe04bd8b40f899e78bee
Sm Bakibillah Lemon <66021119+smb-lemon@users.noreply.github.com>
2021-06-20 23:14:56 +0600

    third

---
+++ LAB_TASK_05_SESSION/login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PUBLIC HOME</title>
+</head>
+<body>
+	<form method="post" action="
+	login.php">
+	<table border="1" width="50%">
+		<tr>
+			<td width="50%">
+				Company
+			</td>
+			<td align="right"><a href="">Home</a>|<a href="">Login</a>|<a href="">Registration</a></td>
+		</tr>
+		<tr>
+		<td colspan="2">
+			<table width="60%">
+		<fieldset>
+			<legend>LOGIN</legend>
+		User name:<input type="text" name="username"><br><br>
+		Password  :<input type="Password" name="Password">
+		<hr><br>
+		<input type="submit" name="" value="Submit"><a href="forgot_password.php">Forgot Password?</a>
+	    </fieldset>
+	</form>
+</body>
+</html>
