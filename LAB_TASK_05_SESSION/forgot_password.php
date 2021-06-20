6e09a3f9db65015a9c7e58cfc065fd7ea3303d44
Sm Bakibillah Lemon <66021119+smb-lemon@users.noreply.github.com>
2021-06-20 23:17:31 +0600

    fourth

---
+++ LAB_TASK_05_SESSION/forgot_password.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>FORGOT PASSWORD</title>
+</head>
+<body>
+	<form method="post" action=""></form>
+	<table border="1" width="50%">
+		<tr>
+			<td width="50%">
+				Company
+			</td>
+			<td align="right"><a href="">Home</a>|<a href="login.php">Login</a>|<a href="registration.php" >Registration</a></td>
+		</tr>
+		<tr>
+			<td colspan="2">
+				<fieldset>
+					<legend>FORGOT PASSWORD</legend>
+					Enter Email:<input type="email" name="Email">
+					<hr>
+			<input type="submit" name="submit" value="Submit">
+				</fieldset>	
+</body>
+</html>
